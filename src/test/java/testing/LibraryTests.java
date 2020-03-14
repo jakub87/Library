80ad7dd4d43f7b44726bfd0f8546f6287efcef6d
jakub87 <jakubcichowski@o2.pl>
2020-03-14 14:04:23 +0100

    added packages

---
+++ src/test/java/testing/LibraryTests.java
@@ -0,0 +1,38 @@
+package testing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class LibraryTests {
+
+
+
+
+    
+   // main main;
+
+
+//
+//    @Test
+//    void dodajTest() {
+////        Assertions.assertEquals(10,main.dodaj(5,5));
+////        Assertions.assertEquals(15,main.dodaj(10,5));
+//
+//        Assertions.assertAll(
+//                () -> Assertions.assertEquals(10,main.dodaj(5,5)),
+//                () -> Assertions.assertEquals(20,main.dodaj(15,5))
+//
+//        );
+//
+//    }
+//
+////    assertAll(
+////                ()-> assertEquals(4,liczby.dodaj(2,2)),
+////            ()-> assertEquals(5,liczby.dodaj(3,2)),
+////            ()-> assertEquals(1,liczby.dodaj(2,-1))
+////
+////            );
+
+}
